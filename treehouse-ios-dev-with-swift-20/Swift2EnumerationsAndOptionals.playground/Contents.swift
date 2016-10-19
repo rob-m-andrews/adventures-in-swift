12d871d53761527ec2ea82745f7251435c3df654
Rob Andrews <andrews.robert.m@gmail.com>
2016-10-19 14:11:03 -0400

    Adds Swift 2.0 Enumerations Playgrounds Notes - Part 3

--- treehouse-ios-dev-with-swift-20/Swift2EnumerationsAndOptionals.playground/Contents.swift
+++ treehouse-ios-dev-with-swift-20/Swift2EnumerationsAndOptionals.playground/Contents.swift
@@ -97,9 +97,53 @@
 // Soon, we'll learn better ways to work with this optional type.
 
 ////////////////////////////////////////////////////////
-// Part 3
+// Part 3: Optional Binding
 ////////////////////////////////////////////////////////
 
+// If you'll remember Dictionaries always return an optional value.
 
 
+let airportCodes = ["CDG" : "Charles de Gaulle"]
 
+// Now, get a value out, using a key that we know doesn't exist.
+let newYorkAirport = airportCodes["JFK"]
+
+// You can see in the results area in Swift Playgrounds, the value is nil.
+// What if we didn't know what the contents of 'airportCodes' was, and 
+// had no idea if JFK existed as a key in the dict?
+
+// We could go about checking it this way
+// This is called optional binding. We do this to find out if an optional contains a value
+if let airportNewYork = airportCodes["JFK"] {
+    // bind to a temporary constant we create in the if statement
+    // This temp constant can only be used inside the if statement block
+    print(airportNewYork)
+} else {
+    print("That key doesn't exist!")
+}
+
+
+// The difference between the force unwrapping and optional binding is, we let
+// the compiler do the unwrapping for you
+
+// Many optional binds can be included in ONE if statement.
+
+// Example -- Take this more complicated structure
+let weatherDictionary: [String : [String : String]] = ["currently": ["temp": "22.3"], "daily": ["temp": "22.3"], "weekly": ["temp": "22.3"]]
+
+// This code below works, but it's really freaking ugly... It looks like a pyramid when turned sideways
+// The swift community calls this the 'optional pyramid of doom' :)
+
+//if let dailyWeather = weatherDictionary["daily"] {
+//    if let highTemperature = dailyWeather["temp"] {
+//        print(highTemperature)
+//    }
+//}
+
+// This code looks _slightly_ better.
+if let dailyWeather = weatherDictionary["daily"],
+    let highTemperature = dailyWeather["temp"] {
+    print(highTemperature)
+}
+
+// There are some downsides to the approach above, that we'll soon learn.
